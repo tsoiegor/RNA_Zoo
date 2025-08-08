69144895f0dbfb55577a58ea4312b2aa02038d66
Egor Tsoi <tsoies@hydra-eye1.ipa.icgbio.ru>
2025-08-08 17:34:28 +0700

    adding data

---
+++ scripts/pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+align_full_database () {
+    PATH_TO_FASTQ = $1
+    OUT_BAM_PATH = $2
+}
+
+make_barcode_files () {
+    OUT_DIR_WITH_BARCODES = $1
+
+}
+
+split_fastq_by_species () {
+    OUT_DIR = $1
+    DIR_WITH_BARCODES=$2
+    
+}
+
+
+
+
