aa6ca01d5fb8d5b1b5beeb4a0919e0a3267e3407
Egor Tsoi <tsoies@hydra-eye1.ipa.icgbio.ru>
2025-08-22 13:27:07 +0700

    bug fixes

--- scripts/split_fastq.py
+++ scripts/split_fastq.py
@@ -14,7 +14,9 @@ import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from pandarallel import pandarallel
 
+pandarallel.initialize(nb_workers=11)
 
 class splitFastq():
     def __init__(self, 
@@ -32,19 +34,15 @@ class splitFastq():
         
     def make_barcode2readName_dict(self) -> dict:
         print('********** make_barcode2readName_dict **********', flush=True)
-        barcode2readName_dict = {}
+        barcode2readName_dict = defaultdict(list)
         with open(self.barcode_fq_csv, mode='r') as infile:
             reader = csv.reader(infile)
             for rows in reader:
                 read_name = rows[0]
                 barcode = rows[1][0:20]
-                barcode2readName_dict[barcode] = read_name
-        print('[make_barcode2readName_dict]: DONE')
+                barcode2readName_dict[barcode].append(read_name)
+        print('[make_barcode2readName_dict]: DONE', flush=True)
         return barcode2readName_dict
-
-    @staticmethod
-    def barcodes2readnames(barcodes: list, barcode2readName_dict: dict) -> list:
-        return [barcode2readName_dict[barcode] for barcode in barcodes]
     
     def make_sp2barcodes(self):
         def write_barcode_lists(row, out_path=self.out_path):
@@ -52,32 +50,36 @@ class splitFastq():
             barcodes = pd.DataFrame({'barcode': row.values[1]})
             out_file = os.path.join(out_path, filename)
             barcodes.to_csv(out_file, header=False, index=False)
-        barcode2sp = pd.read_csv(self.barcode2sp)
+        barcode2sp = pd.read_csv(self.barcode2sp, index_col=0)
+        print(f'[make_sp2barcodes]: barcode2sp df loaded, columns: {barcode2sp.columns}', flush=True)
         sp2barcodes = barcode2sp.groupby(by='species').agg(lambda x: ','.join(x).split(',')).reset_index()
+        print(f'[make_sp2barcodes]: sp2barcodes groupby df loaded, columns: {sp2barcodes.columns}', flush=True)
         sp2barcodes.apply(write_barcode_lists, axis=1)
+        print(f'[make_sp2barcodes]: DONE', flush=True)
         return sp2barcodes
     
     def sp2barcodes_to_sp2readNames(self) -> pd.DataFrame:
-        barcode2readName_dict = self.make_barcode2readName_dict()
         sp2barcodes = self.make_sp2barcodes()
-        p = Pool(self.processes)
-        sp2barcodes['barcode'] = p.map(self.barcodes2readnames(barcode2readName_dict=barcode2readName_dict), sp2barcodes['barcode'])
+        barcode2readName_dict = self.make_barcode2readName_dict()
+        print('[sp2barcodes_to_sp2readNames]: sp2barcodes and barcode2readName_dict loaded', flush=True)
+        sp2barcodes['barcode'] = sp2barcodes['barcode'].apply(lambda barcodes: [x for xs in [barcode2readName_dict[barcode] for barcode in barcodes] for x in xs if type(xs) == list])
         return sp2barcodes
     
     def writeFastq(self):
         def make_fastq(row):
-            out_file = row[0]
-            readNames_set = set(row[1])
+            out_file = row.iloc[0]
+            readNames_set = set(row.iloc[1])
             with open(self.fastq, "r") as infastq, open(out_file, "w") as outfastq:
-                for record in SeqIO.parse(infastq, "fastq"):
-                    if record.id.replace('/1', '/2') in readNames_set:
+                for record in tqdm(SeqIO.parse(infastq, "fastq"), total=770731614):
+                    if record.id.replace('/2', '/1') in readNames_set:
                         outfastq.write(f"@{record.id}\n")
                         outfastq.write(f"{record.seq}\n")
                         outfastq.write("+\n")
-                        outfastq.write(f"{record.letter_annotations['phred_quality']}\n")
+                        outfastq.write(f"{''.join([chr(i+33) for i in record.letter_annotations['phred_quality']])}\n")
         sp2readNames = self.sp2barcodes_to_sp2readNames()
-        p = Pool(self.processes)
-        p.map(lambda df: df.apply(make_fastq, axis=1), sp2readNames)
+        sp2readNames['species'] = sp2readNames.species.apply(lambda x: os.path.join(self.out_path, x+'.fastq')).to_list()
+        print('[writeFastq]: writing fastq...', flush=True)
+        sp2readNames.parallel_apply(make_fastq, axis=1)
         print('DONE', flush=True)
 
 
@@ -90,6 +92,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--barcode_fq_csv', type=str)
     
     args = parser.parse_args()
+    print('arguments are loaded...', flush=True)
     
     splitFastq(args.out_path, args.barcode_fq_csv, args.barcode2sp, args.fastq).writeFastq()
     
